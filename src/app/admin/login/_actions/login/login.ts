6a91587c86721cbda3300c8586718927a6599607
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-12-26 21:49:45 +0900

    ログインをリファクタリング

--- src/app/admin/login/_actions/login/login.ts
+++ src/app/admin/login/_actions/login/login.ts
@@ -5,16 +5,8 @@ type LoginParams = { email: string; password: string };
 export async function login(loginParams: LoginParams): Promise<Result> {
   const client = await createClient();
 
-  try {
-    const result = await client.auth.signInWithPassword(loginParams);
-    if (result.error) {
-      return {
-        success: false,
-        message: "メールアドレス、またはパスワードが間違っています",
-      };
-    }
-    return succeed();
-  } catch (error) {
-    return fail("ログイン時にエラーが発生しました。時間をおいてお試しください");
-  }
+  const result = await client.auth.signInWithPassword(loginParams);
+  return result.error
+    ? fail("メールアドレス、またはパスワードが間違っています")
+    : succeed();
 }
