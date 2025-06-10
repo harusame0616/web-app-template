46347b81ab19d9337b6ec63c5caedc5af6b79bf6
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-06-10 22:13:44 +0900

    ログインブロック追加

--- apps/web/src/app/admin/login/page.tsx
+++ apps/web/src/app/admin/login/page.tsx
@@ -1,18 +1,19 @@
-import { Metadata } from "next";
+import { GalleryVerticalEnd } from "lucide-react"
 
-import { LoginCard } from "./_components/login-card";
+import { LoginForm } from "@/components/login-form"
 
-export const metadata: Metadata = {
-  title: "ログイン",
-};
-
-export default function Page() {
+export default function LoginPage() {
   return (
-    <div className="flex h-full flex-col items-center p-4">
-      <div className="max-h-[120px] grow" />
-      <div className="w-full max-w-sm">
-        <LoginCard />
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Demo App
+        </a>
+        <LoginForm />
       </div>
     </div>
-  );
+  )
 }
