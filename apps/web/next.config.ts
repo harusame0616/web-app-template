033a12e2725ee5faf07fdd539cbf8ec748b04f1b
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-06-11 06:50:26 +0900

    next.config.tsにESLintとTypeScriptのビルドエラー無視設定を追加。デバッグ用デプロイを考慮。

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 型チェックや静的解析は ci で行うので、ビルド時には無視する
+  // デバッグ用にとりあえずデプロイしたいなどのケースも有るため
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
