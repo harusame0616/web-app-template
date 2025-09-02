e3fe05f6f5b485b308343fde0347bc0c232f6df1
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-09-03 06:53:41 +0900

    web 削除

--- apps/web/src/features/auth/logout/actions.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { createServerAction } from "@workspace/libs/server-action/server";
-
-import { logout } from "./logout";
-
-export const logoutAction = createServerAction(logout);
